0d58e6e60aa715d9a1becd4795893c5061a2184e
Mike Gran <spk121@yahoo.com>
2017-05-23 07:14:36 -0700

    hack

--- mlg/query_color.sh
+++ mlg/query_color.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-TERMS=`ls -1 /usr/share/terminfo/*/* |\
-    xargs basename -a`
+TERMS=$(ls -1 /usr/share/terminfo/*/* | xargs basename -a)
 echo "("
 for t in $TERMS
 do
-    C=`tput -T$t colors`
-    if [ $C -gt 7 ] ; then
-	F=`tput -T$t setaf`
-	if [ $F = `tput -Tansi setaf` ] ; then
+    C=$(tput -T"$t" colors)
+    if [ "$C" -gt 7 ] ; then
+	F=$(tput -T"$t" setaf)
+	if echo "$F" | sed -n '/%p1%d[;m]/q0' ; then
 	    echo "\"$t\""
 	fi
     fi
