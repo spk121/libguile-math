35597f37107be99d3f12678125571a7e0f9801d0
Michael Gran <spk121@yahoo.com>
2019-12-30 00:55:14 -0800

    Refactor string-sed-substitute and eliminate infinite loop

--- module/mlg/strings.scm
+++ module/mlg/strings.scm
@@ -88,6 +88,109 @@ such character."
           ;; else
           (string-ref str i))))
 
+(define (_sed-substitute String MatchStruct ReplacementString)
+  "Given a String and a MatchStruct, replace the matching characters
+in String with ReplacementString, handling any escape sequences."
+  ;; Handle the escape sequences in the replacement string
+  (let loop ((j 0) (rstr ""))
+    (let ((c (string-ref-safe ReplacementString j)))
+      (cond
+       ;; If we've reached the end of the replacement string,
+       ;; we're done.
+       ((char=? c #\null)
+        rstr)
+
+       ;; Backslash escapes in the replacement string, so look
+       ;; ahead for the next character.
+       ((char=? c #\\)
+        (let ((c2 (string-ref-safe ReplacementString (1+ j))))
+          (cond
+           ((char=? c2 #\null)
+            ;; Backslash at the end of the string is used as a
+            ;; literal backslash.  We're done.
+            (string-append rstr "\\"))
+
+           ((char->numeric-value c2)
+            ;; Backslash + digit means we're going to include
+            ;; the Nth submatch here, then keep looping.
+            (let ((n (char->numeric-value c2)))
+              (loop (+ 2 j)
+                    (string-append
+                     rstr
+                     (if (< n (match:count MatchStruct))
+                         (match:substring MatchStruct n)
+                         ;; else
+                         "")))))
+
+           ((char=? c2 #\n)
+            ;; Backslash + n means we're going to
+            ;; put a real newline here.
+            (loop (+ 2 j)
+                  (string-append rstr "\n")))
+
+           (else
+            ;; Backslash + anything else is an escape?  FIXME:
+            ;; check if it is really an escape
+            (loop (+ 2 j)
+                  (string-append
+                   rstr
+                   (string c2)))))))
+
+       ((char=? c #\&)
+        ;; Ampersand is replaced by the entire match
+        (loop (1+ j)
+              (string-append
+               rstr
+               (match:substring MatchStruct))))
+
+       (else
+        ;; Otherwise, this character is not an escape
+        ;; character.
+        (loop (1+ j)
+              (string-append
+               rstr
+               (string c))))))))
+
+(define (_string-sed-substitute-one String Matches ReplacementString Occurrence)
+  "Given a list of Matches, this replaces the Nth matching occurrence
+in String with ReplacementString, handling any possible escapes in
+ReplacementString."
+  (let ((match-struct (list-ref Matches (1- Occurrence)))) ; sed matches are 1-indexed
+    (string-append
+     (match:prefix match-struct)
+     (_sed-substitute String match-struct ReplacementString)
+     (match:suffix match-struct))))
+
+(define (_string-sed-substitute-global String Matches ReplacementString)
+  "Given a list of Matches, this replaces all the  matching occurrences
+in String with ReplacementString, handling any possible escapes in
+ReplacementString."
+  (let loop ((i 0) (output ""))
+    (cond
+
+     ;; The 1st iteration, the prefix is from the beginning
+     ;; to the matching string.
+     ((= i 0)
+      (loop 1
+            (string-append
+             (match:prefix (first Matches))
+             (_sed-substitute String (first Matches) ReplacementString))))
+
+     ;; For other iterations, the prefix is in between the matching
+     ;; strings.
+     ((< i (length Matches))
+      (loop (1+ i)
+            (string-append
+             output
+             (substring String
+                        (match:end (list-ref Matches (1- i)))
+                        (match:start (list-ref Matches i)))
+             (_sed-substitute String (list-ref Matches i) ReplacementString))))
+
+     ;; To finish, add the suffix after the last match
+     ((= i (length Matches))
+      (string-append output (match:suffix (last Matches)))))))
+
 (define (string-sed-substitute String RegexString ReplacementString FlagsString)
   "Given a regular expression string, a replacement string,
 and flags, return a new string modified by the given subsitututon.
@@ -96,115 +199,35 @@ An amersand '&' in the replacement string shall be replaced by the
 string matching the regular expression string.  Backslash + digit
 in the replacement string shall be replaced by the corresponding
 back reference in the regular expression string, or by an empty
-string if there is no norresponding back reference.
+string if there is no corresponding back reference.
 
 Flags is a string consisting of one or more of
 - 'g' - globally substitute all non-overlapping instances of the regular
   expression.
 - '1' to '9' - substitute only the Nth occurrence of the regular expression
 "
-  (let ((global? (string-contains FlagsString "g"))
-        (count (string-get-first-digit FlagsString)))
-
-    ;; Unless the user has explicitly requested a count, we assume
-    ;; that we're matching the first match, or, if a 'g' flag was
-    ;; given, we matching on all matches.
-
-    (when (not count)
-      (set! count 1))
-    (let loop ((i 1)
-               (start 0)
-               (output ""))
-      (let ((match-struct (string-match RegexString String start)))
-        ;; (log-debug-locals)
-        (cond
-         ((not match-struct)
-          ;; There is no match, so append the remainder of the input
-          ;; string to the output string, and exit.
-          (string-append output
-                         (string-drop String start)))
-
-         ((and (not global?)
-               (not (= i count)))
-          ;; Here were skipping this match occurrence, so copy
-          ;; the string that made a match to the output, and loop.
-          (loop (1+ i)
-                (match:end match-struct)
-                (string-append output
-                               (substring String
-                                          start
-                                          (match:end match-struct)))))
-
-         (else
-          ;; We've made a match, so create a replacement string.
-          (let ((replaced-string
-                 (let loop2 ((j 0)
-                             (rstr (substring String
-                                              start
-                                              (match:start match-struct))))
-                   (let ((c (string-ref-safe ReplacementString j)))
-                     ;; (log-debug-locals)
-                     (cond
-                      ((char=? c #\null)
-                       rstr)
-                      ((char=? c #\\)
-                       (let ((c2 (string-ref-safe ReplacementString (1+ j))))
-                         (cond
-                          ((char=? c2 #\null)
-                           ;; Backslash at the end of the string is used
-                           ;; as a literal backslash.  We're done.
-                           (string-append rstr "\\"))
-
-                          ((char->numeric-value c2)
-                           ;; Backslash + digit means we're going to
-                           ;; include the Nth submatch here, then keep
-                           ;; looping.
-                           (let ((n (char->numeric-value c2)))
-                             (loop2 (+ 2 j)
-                                    (string-append
-                                     rstr
-                                     (if (< n (match:count match-struct))
-                                         (substring String
-                                                    (match:start match-struct n)
-                                                    (match:end match-struct n))
-                                         ;; else
-                                         "")))))
-
-                          ((char=? c2 #\n)
-                           ;; Backslash + n means we're going to
-                           ;; put a real newline here.
-                           (string-append rstr "\n"))
-
-                          (else
-                           ;; Backslash + anything else is an escape?
-                           ;; FIXME: check if it is really an escape
-                           (loop2 (+ 2 j)
-                                  (string-append
-                                   rstr
-                                   (format #f (string #\\ c2))))))))
-
-                      ((char=? c #\&)
-                       ;; Ampersand is replaced by the entire match
-                       (loop2 (1+ j)
-                              (string-append
-                               rstr
-                               (substring String
-                                          (match:start match-struct)
-                                          (match:end match-struct)))))
-
-                      (else
-                       ;; Otherwise, this character is not an escape
-                       ;; character.
-                       (loop2 (1+ j)
-                              (string-append
-                               rstr
-                               (string c)))))))))
-            ;; Append the replace string, and return to the main loop.
-            (loop (1+ i)
-                  (match:end match-struct)
-                  (string-append
-                   output
-                   replaced-string)))))))))
+  ;; First, find all the matches.
+  (let ((matches (list-matches RegexString String)))
+    (if (null? matches)
+        ;; No matches found, so just return the input string.
+        String
+        ;; else
+        (let ((global? (and (string-contains FlagsString "g")
+                            (> (length matches) 1)))
+              (occurrence (or (string-get-first-digit FlagsString) 1)))
+          ;; Unless the user has explicitly requested a specific
+          ;; occurrence, we assume that we're matching the first
+          ;; match, or, if a 'g' flag was given, we matching on all
+          ;; matches.
+          (if (and (not global?) (< (length matches) occurrence))
+              ;; Not enough matches found, so just return the input
+              ;; string.
+              String
+              ;; else
+              (if global?
+                  (_string-sed-substitute-global String matches ReplacementString)
+                  ;; else
+                  (_string-sed-substitute-one String matches ReplacementString occurrence)))))))
 
 (define (string-starts-with? str char)
   "Return #t is the first character in string STR
